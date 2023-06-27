6fd3533754a9bfe2812ced1acc06e50a74f3f253
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-06-27 15:19:12 +0400

    fetch data via fetch

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 
 import { Container } from '../../layouts'
 
-import  threadsStore  from 'store/threadsStore'
-
+import threadsStore from 'store/threadsStore'
 
 const store = new threadsStore()
 console.log(store)
 
+const handleFetchData = () => {
+  console.log('running')
+  fetch('https://www.reddit.com/r/reactjs.json')
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+}
+
+
 const HomePage = () => {
+
   return (
     <Container>
       <h1 className='ui-title-1'>Home Page</h1>
+      <button onClick={handleFetchData}>fetch</button>
     </Container>
   )
 }
